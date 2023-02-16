0166b2aa7bd5e918d54cfd858d63068100598b17
Pablo Y. Prado <93798700+pyurips@users.noreply.github.com>
2023-02-16 05:53:31 -0300

    Trying to resolve typescript error

--- App.tsx
+++ App.tsx
@@ -1,20 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, TextInput, View, Keyboard, Animated} from 'react-native';
 
 export default function App() {
+  const [loginInput, setLoginInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+  const [loginInputStatus, setLoginInputStatus] = useState(true);
+  const [passwordInputStatus, setPasswordInputStatus] = useState(false);
+  const [inputMarginBottom, setInputMarginBottom] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', () => {
+      //setInputMarginBottom(50);
+    });
+
+    Keyboard.addListener('keyboardDidHide', () => {
+      //setInputMarginBottom(0);
+    });
+  }, []);
+
+  useEffect(() => {
+    Animated.timing(inputMarginBottom, {
+      toValue: 1,
+      duration: 10000,
+      useNativeDriver: true,
+    }).start();
+  }, [inputMarginBottom]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <Animated.View style={styles.container}>
+      {
+        loginInputStatus &&
+        <TextInput
+          style={[styles.inputStyle, {marginBottom: inputMarginBottom}]}
+          autoFocus={true}
+          onChangeText={setLoginInput}
+          value={loginInput}
+          maxLength={40}
+          onSubmitEditing = {(e) => {
+            console.log('Testing submit');
+          }}
+        />
+      }
+
+      {
+        passwordInputStatus &&
+        <TextInput
+          style={styles.inputStyle}
+          autoFocus={true}
+          onChangeText={setPasswordInput}
+          value={passwordInput}
+          maxLength={40}
+        />
+      }
+
       <StatusBar style="auto" />
-    </View>
+    </Animated.View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#212121',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputStyle: {
+      width: 300,
+      padding: 5,
+      borderBottomColor: '#DCDCDC',
+      borderBottomWidth: 2,
+      color: 'white',
+      transitionProperty: "all",
+      transitionDuration: "2000ms"
+  }
 });
